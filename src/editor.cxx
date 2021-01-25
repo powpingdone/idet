09f7986464cdc430a6dec2d5ee476a070368ac0f
Aidan Case <aidanzcase@gmail.com>
2021-01-25 00:19:25 -0600

    Get basic text editor skeleton.

--- src/editor.cxx
+++ src/editor.cxx
@@ -1,5 +1,22 @@
 #include "editor.h"
+#include <gtkmm/enums.h>
 
-editorWindow::editorWindow() {
+editorWindow::editorWindow() : slaveEditor(Gtk::Orientation::HORIZONTAL) {
     set_title("editorWindow");
+    
+    // setup box containing the other component boxes
+    masterGrid.set_margin(10);
+    set_child(masterGrid);
+
+    // setup editor box
+    masterGrid.attach(sourceHolder, 0, 0);
+    sourceHolder.set_child(slaveEditor);
+    slaveEditor.set_start_child(sourceLines);
+    slaveEditor.set_resize_start_child(false);
+    slaveEditor.set_wide_handle(false);
+    slaveEditor.set_position(48);
+    slaveEditor.set_end_child(textView);
+    sourceLines.set_editable(false);
+    sourceHolder.set_expand();
+
 }
--- src/editor.h
+++ src/editor.h
@@ -1,11 +1,21 @@
 #ifndef EDITOR_H
 #define EDITOR_H
 
+#include <gtkmm/enums.h>
 #include <gtkmm/window.h>
+#include <gtkmm/textview.h>
+#include <gtkmm/scrolledwindow.h>
+#include <gtkmm/grid.h>
+#include <gtkmm/paned.h>
 
 class editorWindow : public Gtk::Window {
     public:
         editorWindow();
+    private:
+        Gtk::Grid masterGrid; // contains rest of boxes
+        Gtk::Paned slaveEditor; // contains textviews relating to actual code editing
+        Gtk::ScrolledWindow sourceHolder;
+        Gtk::TextView textView, sourceLines;
 };
 
 #endif //EDITOR_H
